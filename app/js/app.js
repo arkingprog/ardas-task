580f69098bc4e6c994270c4c002cc294419fe3db
arkingprog <arking@ukr.net>
2016-04-07 13:44:26 +0300

    refactoring

--- app/js/app.js
+++ app/js/app.js
@@ -1,23 +1,23 @@
-'use strict'
-var app=angular.module('todoApp',['ngRoute',"xeditable"]);
-
-app.config(function($routeProvider){
-    $routeProvider
-        .when('/tasks',
-        {
-            templateUrl:'./views/tasks.html',
-            controller:'tasksCtrl'
+(function(){
+    'use strict'
+    angular.module('todoApp',['ngRoute',"xeditable"])
+        .config(function($routeProvider){
+            $routeProvider
+                .when('/tasks',
+                    {
+                        templateUrl:'./views/tasks.html',
+                        controller:'tasksCtrl'
+                    })
+                .when('/task/:id',
+                    {
+                        templateUrl:'./views/task.html',
+                        controller:'taskCtrl'
+                    })
+                .otherwise({
+                    redirectTo:'/tasks'
+                });
         })
-        .when('/task/:id',
-        {
-            templateUrl:'./views/task.html',
-            controller:'taskCtrl'
-        })
-        .otherwise({
-            redirectTo:'/tasks'
-        });
-});
-
-app.run(function(editableOptions) {
-    editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
-});
+        .run(function(editableOptions) {
+            editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+    });
+})();
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,20 +1,21 @@
-'use strict'
-app.controller('tasksCtrl',function($scope,Tasks){
-    var tasks=Tasks.getTask();
-    tasks.then(function(value){
-        $scope.tasks=value.data;
-    });
-});
-
-app.controller('taskCtrl',function($scope,$routeParams,Tasks){
-    var tasks=Tasks.getTask();
-    tasks.then(function(value){
-        value.data.forEach(function(item){
-          if(item.id==$routeParams.id) $scope.task=item;
+(function(){
+    'use strict'
+    angular.module('todoApp')
+        .controller('tasksCtrl',function($scope,Tasks){
+            var tasks=Tasks.getTask();
+            tasks.then(function(value){
+                $scope.tasks=value.data;
+            });
         })
+        .controller('taskCtrl',function($scope,$routeParams,Tasks){
+        var tasks=Tasks.getTask();
+            tasks.then(function(value){
+                value.data.forEach(function(item){
+                    if(item.id==$routeParams.id) $scope.task=item;
+                })
+            });
+            $scope.updateTask=function(id,name){
+                Tasks.updateTask(id,name);
+            }
     });
-    $scope.updateTask=function(id,name){
-        Tasks.updateTask(id,name);
-    }
-
-});
+})();
