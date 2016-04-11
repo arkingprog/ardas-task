4dbc667088c3f99f4a66a5b42b7d0e8d99cd414c
arkingprog <arking@ukr.net>
2016-04-11 13:54:04 +0300

    editable task name

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,19 +3,41 @@
     angular.module('todoApp')
         .controller('tasksCtrl',function($scope,Tasks){
             var tasks=Tasks.getTask();
+
             tasks.then(function(value){
                 $scope.tasks=value.data;
             });
+
         })
         .controller('taskCtrl',function($scope,$routeParams,Tasks){
-        var tasks=Tasks.getTask();
+            var tmpName='';
+
+            $scope.isEditableTask=false;
+
+            $scope.editable=function(){
+                $scope.isEditableTask=!$scope.isEditableTask;
+                tmpName=$scope.task.name;
+            };
+
+            $scope.cancel=function(){
+                $scope.isEditableTask=!$scope.isEditableTask;
+                $scope.task.name=tmpName;
+            };
+
+            $scope.updateTask=function(){
+                $scope.isEditableTask=!$scope.isEditableTask;
+                Tasks.updateTask($scope.task.id,$scope.task.name);
+            };
+
+            var tasks=Tasks.getTask();
+
             tasks.then(function(value){
                 value.data.forEach(function(item){
                     if(item.id==$routeParams.id) $scope.task=item;
                 })
             });
-            $scope.updateTask=function(id,name){
-                Tasks.updateTask(id,name);
-            }
+
+
     });
 })();
+
